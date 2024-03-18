266f16bef82570b172c5f5550fefa63c0f3f5c62
Eliel Moura <124813479+Tiuliel@users.noreply.github.com>
2024-03-18 12:00:08 -0300

    login

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,13 +1,38 @@
-import { Button, StyleSheet, TextInput, View } from "react-native";
+import { Alert, Button, StyleSheet, TextInput, View } from "react-native";
+
+// importando os recursos de autenticação
+import { auth } from "../../firebase.config";
+// importando a função de login com e-mail e senha
+import { signInWithEmailAndPassword } from "firebase/auth";
+
+import { useState } from "react";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const login = async () => {
+    if (!email || !senha) {
+      Alert.alert("Atenção!", "Preencha e-mail e senha!");
+      return;
+    }
+    console.log(email, senha);
+  };
   return (
     <View style={estilos.container}>
       <View style={estilos.formulario}>
-        <TextInput placeholder="E-mail" style={estilos.input} />
-        <TextInput placeholder="Senha" style={estilos.input} secureTextEntry />
+        <TextInput
+          onChangeText={(valor) => setEmail(valor)}
+          placeholder="E-mail"
+          style={estilos.input}
+        />
+        <TextInput
+          onChangeText={(valor) => setSenha(valor)}
+          placeholder="Senha"
+          style={estilos.input}
+          secureTextEntry
+        />
         <View style={estilos.botoes}>
-          <Button title="Entre" color="green" />
+          <Button onPress={login} title="Entre" color="green" />
         </View>
       </View>
     </View>
